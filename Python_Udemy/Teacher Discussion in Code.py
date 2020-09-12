9bced129588bfd71229f5393822bcf1ca5dfe8e5
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-09-12 23:23:21 +0530

    Removed unwanted code
    Removed unwanted code

--- Python_Udemy/Teacher Discussion in Code.py
+++ Python_Udemy/Teacher Discussion in Code.py
@@ -1,5 +1,4 @@
 Teacher=input('Please enter the teacher\'s name: ')
 Subject=input('Please enter the subject the teacher teaches: ')
 Teaching_Style=input('Please enter the teaching style the teacher teaches: ')
-print('Your Madlib:')
 print('Subject\'s the teacher teaches',Teacher,'is ',Subject,'.The teaching style of the ',Teacher,' is ',Teaching_Style,'.')
