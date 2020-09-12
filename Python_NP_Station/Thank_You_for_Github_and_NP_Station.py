3c95a0e3a2537c8eda26cbcefad7151cd635026d
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-09-12 23:53:50 +0530

    Added Email Body

---
+++ Python_NP_Station/Thank_You_for_Github_and_NP_Station.py
@@ -0,0 +1,23 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Signature='Regard\'s,\nPaarin Modi'
+>>> body="""
+Dear Omkar Bhaiya,
+In last 5 days I have completed all that NP Station videos and it was amazing!!!
+I learned all the f_string,Python Maths and etc. in all that Videos.
+Thanks for your help for the beginner videos and thanks for the Github app and I learned the Github with my father from that videos!!!!!!!
+I have also learn more about the coding language 'Python'...and Thanks!!!
+
+"""
+>>> print(f"{body} {Signature}")
+
+Dear Omkar Bhaiya,
+In last 5 days I have completed all that NP Station videos and it was amazing!!!
+I learned all the f_string,Python Maths and etc. in all that Videos.
+Thanks for your help for the beginner videos and thanks for the Github app and I learned the Github with my father from that videos!!!!!!!
+I have also learn more about the coding language 'Python'...and Thanks!!!
+
+ Regard's,
+Paarin Modi
+>>> 
