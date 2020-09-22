9a4f7d69cabb682e82bfa037c5de24725e469ac6
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-09-22 11:17:15 +0530

    My Turtle Graphics with Colors and BgColor
    Colors and BgColor

--- Python_Udemy/Turtle Craphics.py
+++ Python_Udemy/Turtle Craphics.py
@@ -1,5 +1,13 @@
 import turtle
 t=turtle.Pen()
+turtle.bgcolor('black')
+t.speed(0)
+colors = ["green","orange","blue","red","yellow"]
 for x in range(100):
-    t.forward(x)
-    t.left(90)
+    t.pencolor(colors[ x%5] )
+    t.circle(x)
+    t.left(78)
+
+                       
+            
+    
---
+++ Python_Udemy/Turtle Graphics 'Circle'.py
@@ -0,0 +1,7 @@
+# CircleSpirall.py
+import turtle
+t=turtle.Pen()
+t.speed(0)
+for x in range(100):
+    t.circle(x)
+    t.left(60)
