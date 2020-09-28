344e0247897dfd930258fca51e45ef8a2aa52d75
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-09-28 12:11:19 +0530

    Learning Variables

--- Python_Udemy/Python Sides.py
+++ Python_Udemy/Python Sides.py
@@ -2,12 +2,13 @@ import turtle
 t=turtle.Pen()
 turtle.bgcolor("black")
 t.speed(0)
-sides = eval ( input ("Enter a number of sides between 2 and 6: ") )
+sides = 5
 colors = ["blue","red","yellow","orange","green",
-          "white","violet","pink","grey","light blue",
+          "violet","red","pink","grey","light blue",
           "light yellow","light green"]
 for x in range(360):
     t.pencolor(colors[x%sides])
     t.forward(x * 3 / sides + x)
-    t.left(360/sides + 1)
-    t.width(x*sides/100)
+    t.left(360/sides + 95)
+    t.width(x*sides/200)
+ 
