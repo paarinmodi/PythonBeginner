c61fa9131e0d6c896b9fc2ceb14f2cb8d6042a0a
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-10-02 22:44:52 +0530

    Update Thanks_In_Variables.py

--- Python_Udemy/Thanks_In_Variables.py
+++ Python_Udemy/Thanks_In_Variables.py
@@ -1,12 +1,8 @@
 #Thanks.py
 my_name = "Paarin"
 my_age = 9
-my_favorite_cake = "Oreo and Chocolate cake"
-my_favorite_browsers = "Google Chrome,Mozila Firefox,Microsoft Edge"
 your_name = input ("What is your name ?")
 your_age = input ("How old are you,dude ?")
-your_favorite_cake = input ("What are your favorite cake,lounge lizard ?")
-your_favorite_browsers = input("Hey Dude,What are your favorite browsers ?")
-print("My name is", my_name, ", and my super duper age is", my_age, " years old and my favorite cakes are",my_favorite_cake,"my favorite browsers are",my_favorite_browsers,".")
-print("Your name is", your_name, ",and your age is",your_age ,your_favorite_cake,"is your favorite cakes and your favorite browsers are",your_favorite_browsers,".")
+print("My name is", my_name, ", and my super duper age is", my_age,".")
+print("Your name is", your_name, ",and your age is", your_age,".")
 print("Thanks for answering my questions,",your_name ,"!")
