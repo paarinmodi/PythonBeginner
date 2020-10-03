1a21e3bfdd7c0ae27f21e6204445adaeeff30ad4
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-10-03 12:01:57 +0530

    Maths Python with Udemy

---
+++ Python_Udemy/Maths Python.py
@@ -0,0 +1,45 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 42+23+58
+123
+>>> 435-27-120
+288
+>>> 94 * 20
+1880
+>>> 290/21
+13.80952380952381
+>>> 15 /10
+1.5
+SyntaxError: multiple statements found while compiling a single statement
+>>> 4 **2
+16
+>>> 
+>>> 42+23+58
+123
+>>> 435-27-120
+288
+>>> 94 * 20
+1880
+>>> 290?21
+SyntaxError: invalid syntax
+>>> 290/21
+13.80952380952381
+>>> 134/1
+134.0
+>>> 134/2
+67.0
+>>> 5 ** 5
+3125
+>>> (4+2) * 3
+18
+>>> What is 4+3:
+	
+SyntaxError: invalid syntax
+>>> What is 4+3?
+SyntaxError: invalid syntax
+>>> 2+2
+4
+>>> 2+2=
+SyntaxError: invalid syntax
+>>> 
