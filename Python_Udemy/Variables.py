c5a81c07983f341b2e3a76cd5bd6924beb7683cc
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-10-07 11:27:03 +0530

    My Python Variables
    Variables

---
+++ Python_Udemy/Variables.py
@@ -0,0 +1,33 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5657
+>>> x
+5657
+>>> x * 2
+11314
+>>> x / 2
+2828.5
+>>> x - 1000
+4657
+>>> x = 3865
+>>> x
+3865
+>>> x=x-7
+>>> x
+3858
+>>> x=94
+>>> x
+94
+>>> x=x-39
+>>> x=
+SyntaxError: invalid syntax
+>>> x
+55
+>>> x=94
+>>> x
+94
+>>> x=x-32
+>>> x
+62
+>>> 
