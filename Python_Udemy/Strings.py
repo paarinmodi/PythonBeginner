46d2f4c5e32912cc980692949ba34938d6fafa5f
paarinmodi <71181940+paarinmodi@users.noreply.github.com>
2020-11-13 15:03:58 +0530

    Strings

---
+++ Python_Udemy/Strings.py
@@ -0,0 +1,7 @@
+# SayMyName.py
+# Ask the user of for their name
+name=input("What is your name? ")
+# Print their name 100 times
+for x in range (2) :
+    # Print their name followed by a space , not a new line
+    print(name, end = " OYYYYAAAAAYYYYYAAAAAAHHHHHHHHH !! ")
